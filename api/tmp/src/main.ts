0216234093ab632c528a5f3bf525e8bb09a94c57
yaren.ning <yaren.ning@chinapex.com>
2016-09-18 15:51:28 +0800

    remove tmp

--- api/tmp/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { IndexCompoent } from './app/index.component';
-import { appRouterProviders } from './app/app.routers';
-
-bootstrap(IndexCompoent, [appRouterProviders]);
